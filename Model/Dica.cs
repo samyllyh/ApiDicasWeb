c0851002dce679d555b0797b7e6db3afa8509baa
samylly <samyl@DESKTOP-TRVTAH5>
2022-11-13 18:33:52 -0300

    adições das migrations

--- Model/Dica.cs
+++ Model/Dica.cs
@@ -2,7 +2,9 @@
 
 public class Dica
 {
-    public string TituloId { get; set; }
+    public int DicaId { get; set; }
 
-    public string Descrição { get; set; }
+    public string Titulo { get; set; }
+
+    public string? Descrição { get; set; } 
 }
