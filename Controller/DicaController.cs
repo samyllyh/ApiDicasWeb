864a6b564fd7d58fa34c770494e170dbf719e535
Samylly Pires Paulo <samylly.paulo@fitbank.com.br>
2022-11-22 09:29:58 -0300

    adição da classe controller

---
+++ Controller/DicaController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDicasWeb.Controller
+{
+    public class DicaController : ControllerBase
+    {
+
+    }
+}
--- Model/Dica.cs
+++ Model/Dica.cs
@@ -1,10 +1,16 @@
-﻿namespace ApiDicasWeb.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDicasWeb.Model;
 
 public class Dica
 {
     public int DicaId { get; set; }
 
+    [Required]
+    [StringLength(80)]
     public string Titulo { get; set; }
 
+    [Required]
+    [StringLength(int.MaxValue)]
     public string? Descrição { get; set; } 
 }
